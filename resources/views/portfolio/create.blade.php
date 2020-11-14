02e4e4183f729dc07d3ba289ae0b16d7bc8a8b7e
moroccan-coder <tssryassine@gmail.com>
2020-11-14 05:57:13 -0800

    personalise field errors message and change language

--- resources/views/portfolio/create.blade.php
+++ resources/views/portfolio/create.blade.php
@@ -7,28 +7,38 @@
     <div class="row">
         <div class="col-md-12">
 
-            @if (count($errors))
-                <div class="alert alert-danger" role="alert">
-                    <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{$error}}</li>
-                @endforeach
-            </ul>
-                </div>
-            @endif
-
+        
         <form action="{{url('/portfolios')}}" method="POST">
             {{ csrf_field() }}
 
-             <div class="form-groupe">
+             <div class="form-groupe @if($errors->get('title')) has-error @endif">
                  <label for="Title">Title</label>
              <input type="text" name="title" class="form-control" value="{{old('title')}}">
+
+             @if($errors->get('title'))
+                 <ul>
+                @foreach($errors->get('title') as $error)
+                 <li style="color: red;">{{$error}}</li>
+                @endforeach
+                </ul>
+
+             @endif
+
             </div>
             <br>
-            <div class="form-groupe">
+            <div class="form-groupe @if($errors->get('description')) has-error @endif">
                 <label for="Description">Description</label>
 
             <textarea name="description" class="form-control"  cols="30" rows="5">{{old('description')}}</textarea>
+            @if ($errors->get('description'))
+                
+                <ul>
+                @foreach ($errors->get('description') as $error)
+                    <li style="color: red;">{{$error}}</li>
+                @endforeach
+                </ul>
+
+            @endif
         
            </div>
              <br>
